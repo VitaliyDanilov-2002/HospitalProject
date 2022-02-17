fd034c58519ab1b9f61c13ac6e8ba21a33cb329c
VitaliyDanilov-2002 <85956169+VitaliyDanilov-2002@users.noreply.github.com>
2022-02-17 16:30:41 +0200

    Update User.java

--- src/main/java/com/gmail/seedsedowsidushka/hospitalproject/Model/User.java
+++ src/main/java/com/gmail/seedsedowsidushka/hospitalproject/Model/User.java
@@ -11,7 +11,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name="user", schema = "hospitalproject")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
